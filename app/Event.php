a9656ac361faf17cb1202c20109e07ce8a6da1e8
MostefaMourad <m_mostefa@esi.dz>
2020-01-02 07:41:46 +0100

    creation du Model

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'nom' ,  
+        'description',
+        'nbr_interesse',  
+        'nbr_participe',
+        'organisateur',
+        'adresse',
+        'date_debut',
+        'duree',
+    ];
 }
